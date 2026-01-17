e43cceb1a5d9607e0139cda456f9a1a9203524a7
Rejin <rejinrjr144@gmail.com>
2026-01-17 18:00:57 +0530

    Rejin.R.J

--- backend/booking_system/deployment_settings.py
+++ backend/booking_system/deployment_settings.py
@@ -3,8 +3,8 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ.ge('RENDER_EXTERNAL_HOSTNAME')]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -45,7 +45,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get['DATABASE_URL'],
+        default=os.environ['DATABASE_URL'],
         conn_max_age=600,
         
     )}
